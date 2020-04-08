f77374860a18ce61b22157625f186a16fa91ac73
Piotr Filipecki <piotrekfilipecki@gmail.com>
2020-04-08 18:50:40 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
+        response => response.json()
+      )
+      setData(result);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <h1>aa</h1>
       </header>
+      <div className="video-wrapper">
+          {
+            data.map(video => (
+              <div key={video.id}>
+                <h2>{video.name}</h2>
+                <video controls src={video.video_url} />
+              </div>
+            ))
+          }
+      </div>
+
     </div>
   );
 }
